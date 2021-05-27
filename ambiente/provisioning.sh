b31206d40218f2be95dbc7ad25fefffac3752d2d
ValueDriven <junilson@hotmail.com>
2021-05-27 17:38:53 -0300

    Configuração pm2

--- ambiente/provisioning.sh
+++ ambiente/provisioning.sh
@@ -3,27 +3,23 @@
 export NODE_ENV=production
 export HOST=localhost
 export PORT=80
-export BACKEND_HOST=localhost
+export BACKEND_HOST=$HOSTNAME
 export BACKEND_PORT=3000
 
-#Update packages
-#-------------------------
-yum update -y
-
-#Install git
-#-------------------------
+echo "Install git"
+echo "-------------------------"
 yum install git -y
 
-# Install Nginx
-#-------------------------
-#amazon-linux-extras enable nginx1
-#yum install nginx -y
-#chkconfig nginx on
-#service nginx start
-#service nginx status
+echo "Install Nginx"
+echo "-------------------------"
+amazon-linux-extras enable nginx1
+yum install nginx -y
+chkconfig nginx on
+service nginx start
+service nginx status
 
-#Install MySQL
-#-------------------------
+echo "Install MySQL"
+echo "-------------------------"
 yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y
 amazon-linux-extras install epel -y
 yum install mysql-community-server -y
@@ -31,37 +27,38 @@ chkconfig mysqld on
 service mysqld start
 service mysqld status
 
-#Config MySQL and config DB
+echo "Install Node.js"
+echo "-------------------------"
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+yum install -y nodejs
+node --version
+
+echo "Clone APP"
+echo "-------------------------"
+git clone https://github.com/valuedriven/conchayoro.git
+cd conchayoro
+
+echo "Install backend"
+echo "-------------------------"
+cd backend
+npm install
+pm2-runtime ./process.yml
+cd ..
+
+echo "Install frontend"
+echo "-------------------------"
+npm --prefix frontend install
+NODE_ENV=development npm --prefix frontend run build
+cp -r frontend/dist/* /usr/share/nginx/html/
+cp frontend/nginx/default.conf.template /etc/nginx/templates/
+service nginx restart
+
+echo "Config MySQL and config DB"
+echo "-------------------------"
 #grep 'temporary password' /var/log/mysqld.log
 #2020-11-18T15:17:13.884662Z 6 [Note] [MY-010454] [Server] A temporary password is generated for root@localhost: ujtBqhNzE0>8
 #mysql_secure_installation -p'ujtBqhNzE0>8'
 #systemctl enable --now mysqld
 #mysql -uroot -p
 #mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'secretStr0ngPassw0rd!';
-
-# Install Node.js
-# --------------------------
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node --version
-
-#Obter App
-git clone https://github.com/valuedriven/conchayoro.git
-cd conchayoro
-
-#Instalar backend
-cd backend
-npm install
-nohup node src/server.js > /dev/null 2>&1 &
-
-cd ..
-#Instalar frontend
-export NODE_ENV=development
-npm --prefix frontend install
-npm --prefix frontend run build
-export NODE_ENV=production
-cp -r frontend/dist/* /usr/share/nginx/html/
-mkdir /etc/nginx/templates/
-cp frontend/nginx/default.conf.template /etc/nginx/templates/
-service nginx restart
