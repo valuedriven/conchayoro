e229805ce9873691c5fe996a40e9423b099a8bde
JPSouza <junilson@hotmail.com>
2025-04-02 20:03:38 -0300

    Create main.tf

---
+++ .infra/main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "cyo_bucket" {
+  bucket = "cyo_static_bucket"
+  tags = {
+    Group       = "ci_cd"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "cyo_bucket_oc" {
+  bucket = aws_s3_bucket.cyo_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "cyo_bucket_ba" {
+  depends_on = [aws_s3_bucket_ownership_controls.cyo_bucket_oc]
+
+  bucket = aws_s3_bucket.cyo_bucket.id
+  acl    = "public_read"
+}
+
+resource "aws_s3_bucket_policy" "cyo_bucket_policy" {
+  bucket = aws_s3_bucket.cyo_bucket.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::cyo_static_bucket/*"
+    }
+  ]
+}
+POLICY
+}
